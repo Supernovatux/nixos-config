86aabe98cc045ba61c71d93c2042631731a63b46
Thulashitharan <thulashitharan.d@gmail.com>
2024-07-28 14:57:11 +0530

    With added obsidian

--- configuration.nix
+++ configuration.nix
@@ -2,27 +2,32 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
-              boot.loader.systemd-boot.enable = lib.mkForce false;
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+  boot.loader.systemd-boot.enable = lib.mkForce false;
 
-            boot.lanzaboote = {
-              enable = true;
-              pkiBundle = "/etc/secureboot";
-            };
+  boot.lanzaboote = {
+    enable = true;
+    pkiBundle = "/etc/secureboot";
+  };
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.efi.canTouchEfiVariables = true;
 
-   networking.hostName = "supernovatux"; # Define your hostname.
+  networking.hostName = "supernovatux"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
   # Set your time zone.
   time.timeZone = "Asia/Kolkata";
@@ -32,19 +37,25 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
   services.xremap = {
-          /* NOTE: since this sample configuration does not have any DE, xremap needs to be started manually by systemctl --user start xremap */
-          serviceMode = "user";
-          userName = "thulashitharan";
-	  withKDE = true;
+    # NOTE: since this sample configuration does not have any DE, xremap needs to be started manually by systemctl --user start xremap
+    serviceMode = "user";
+    userName = "thulashitharan";
+    withKDE = true;
   };
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
-     font = "Lat2-Terminus16";
-     keyMap = "us";
+    font = "Lat2-Terminus16";
+    keyMap = "us";
   };
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nix.settings.trusted-users = [ "root" "@wheel" ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+  nix.settings.trusted-users = [
+    "root"
+    "@wheel"
+  ];
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
@@ -52,49 +63,52 @@
   programs.kdeconnect.enable = true;
   services.printing.enable = true;
   services.xremap.config.modmap = [
-  {
-     name = "Global";
-     remap = { "KEY_RIGHTALT" = "KEY_BACKSPACE"; }; # globally remap CapsLock to Esc
-   }
- ];
-   services.pipewire = {
-     enable = true;
-     pulse.enable = true;
+    {
+      name = "Global";
+      remap = {
+        "KEY_RIGHTALT" = "KEY_BACKSPACE";
+      }; # globally remap CapsLock to Esc
+    }
+  ];
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.libinput.enable = true;
 
   users.users.thulashitharan = {
-     isNormalUser = true;
-     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-     packages = with pkgs; [
-       firefox
-       tree
-       google-chrome
-       element-desktop-wayland
-       spotify
-       ani-cli
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      firefox
+      tree
+      google-chrome
+      element-desktop-wayland
+      spotify
+      ani-cli
       qbittorrent
+      obsidian
       mpv
 
-     ];
+    ];
   };
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
   environment.systemPackages = with pkgs; [
-     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-     wget
-     git
-     neovim
-     gcc
-     rustup
-     gcc 
-     gdb
-     gnumake
-     sbctl
-     efitools
-     efibootmgr
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    git
+    neovim
+    gcc
+    rustup
+    gcc
+    gdb
+    gnumake
+    sbctl
+    efitools
+    efibootmgr
     gitui
     silver-searcher
     fastfetch
@@ -103,7 +117,7 @@
     gh
     nixfmt-rfc-style
     just
-   ];
+  ];
 
   fonts.packages = with pkgs; [
     noto-fonts-cjk
@@ -113,15 +127,20 @@
     libertine
     dejavu_fonts
     inconsolata
-    (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
+    (nerdfonts.override {
+      fonts = [
+        "FiraCode"
+        "DroidSansMono"
+      ];
+    })
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
   programs.gnupg.agent = {
-     enable = true;
-     enableSSHSupport = true;
+    enable = true;
+    enableSSHSupport = true;
   };
 
   # List services that you want to enable:
@@ -159,4 +178,3 @@
   system.stateVersion = "24.05"; # Did you read the comment?
 
 }
-
